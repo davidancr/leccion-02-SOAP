b2d366874838aea01729098da0cf2de6c6bc61b7
davidancr <67611862+davidancr@users.noreply.github.com>
2024-10-17 02:06:21 +0000

    addSoapService

---
+++ AuthorServiceContract.cs
@@ -0,0 +1,20 @@
+
+using System.Diagnostics;
+using System.ServiceModel;
+using System.Xml.Linq;
+namespace SoapCore_Demo
+{
+    [ServiceContract]
+    public interface IAuthorService
+    {
+        [OperationContract]
+        void MySoapMethod(XElement xml);
+    }
+    public class AuthorService : IAuthorService
+    {
+        public void MySoapMethod(XElement xml)
+        {
+            Trace.WriteLine(xml.ToString());
+        }
+    }
+}
